7c4ceb3486be5267d0e296af7a66d8ced455b29a
Dhruva Srinivas <66675022+dhruvaS-hub@users.noreply.github.com>
2021-02-07 20:12:26 +0530

    Randomize generation with srand() and clean out code.

--- GuessGame/game_runner.h
+++ GuessGame/game_runner.h
@@ -1,42 +1,51 @@
-#include "game_arrays.h"
+#include<random>
+#include<ctime>
 
-// Global Variables
-int maxTries = 5;
-
-void TakePlayerGuess(int maxLimit, int options[50]) {
+void TakePlayerGuess(int maxLimit, int maxTries, int answer) {
 	int choice;
 	int tries = 0;
-	int answer = options[rand() % maxLimit];
 	
 	while (maxTries <= 5) {
-		if (tries == 5) {
-			cout << "You are out of tries. You lose." << endl;
-			break;
-		} else {
-			cout << "Guess a number between 1 - " << maxLimit << ": " << endl;
-			cin >> choice;
 
-			tries++;
+		cout << "Guess a number between 1 - " << maxLimit << ": " << endl;
+		cin >> choice;
 
-			if (choice > maxLimit || choice < 1) {
-				cout << "Invalid Guess. Quitting the game...";
-			}
-			else {
+		tries++;
+
+		if (choice > maxLimit || choice < 1) {
+			cout << "Invalid Guess." << endl;
+		}
+		else {
+			if (tries == maxTries + 1) {
+				cout << "You are out of tries. The answer was : " << answer << endl;
+				break;
+
+			} else {
 				if (choice == answer) {
 					cout << "That's right! You got it in " << tries << " tries.";
 					break;
 				}
 				else {
 					if (choice < answer) {
-						cout << "Your guess is wrong! Hint: The number is greater than your choice." << endl;
+						cout << "Your guess is wrong! Hint: The number is greater than your choice. Tries left: " << maxTries - tries << endl;
 					}
 					else {
-						cout << "Your guess is wrong! Hint: The number is smaller than your choice." << endl;
+						cout << "Your guess is wrong! Hint: The number is smaller than your choice. Tries left: " << maxTries - tries << endl;
 					}
 				}
 			}
 		}
 	}
+}
+
+// Generate random values
+int GenerateRandomValue(int lowestValue, int highestValue) {
+	int randomValue;
+	int range = (highestValue - lowestValue) + 1;
+	srand(time(NULL));
+	randomValue = lowestValue + rand() % range;
+
+	return randomValue;
 }
 
 // Main Game function
@@ -46,19 +55,19 @@ void Game() {
 	cin >> level;
 
 	if (level == 1) {
-		TakePlayerGuess(10, Lvl1Array);
+		TakePlayerGuess(10, 5, GenerateRandomValue(1, 10));
 	}
 	else if (level == 2) {
-		TakePlayerGuess(20, Lvl2Array);
+		TakePlayerGuess(20, 4, GenerateRandomValue(1, 20));
 	}
 	else if (level == 3) {
-		TakePlayerGuess(30, Lvl3Array);
+		TakePlayerGuess(30, 3, GenerateRandomValue(1, 30));
 	}
 	else if (level == 4) {
-		TakePlayerGuess(40, Lvl4Array);
+		TakePlayerGuess(40, 2, GenerateRandomValue(1, 40));
 	}
 	else if (level == 5) {
-		TakePlayerGuess(50, Lvl5Array);
+		TakePlayerGuess(50, 2, GenerateRandomValue(1, 50));
 	}
 	else {
 		cout << "Choose a level between 1-5 ONLY." << endl;
